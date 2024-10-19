612aadedeeb1c39ec6a838c491fd1309ec0e5d3d
ashutosh <vkrypto@gmail.com>
2024-10-20 02:21:25 +0530

    auto syncing..

--- load-testing/k6-grafana/k6/run_docker.js
+++ load-testing/k6-grafana/k6/run_docker.js
@@ -5,10 +5,10 @@ import { check } from 'k6';
 export let options = {
     stages: [
         // Ramp-up from 1 to 5 VUs in 10s
-        { duration: "10s", target: 5 },
+        { duration: "10s", target: 50000 },
 
         // Stay at rest on 5 VUs for 5s
-        { duration: "5s", target: 5 },
+        { duration: "5s", target: 100000 },
 
         // Ramp-down from 5 to 0 VUs for 5s
         { duration: "5s", target: 0 }
